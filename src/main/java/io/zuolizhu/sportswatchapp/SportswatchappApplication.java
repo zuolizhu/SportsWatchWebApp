1e58389da4aed68cd9bafb258eac3ac53cc71289
Zuoli <zuolizhu5960x@gmail.com>
2018-12-04 13:53:45 -0500

    make login button looks better

--- src/main/java/io/zuolizhu/sportswatchapp/SportswatchappApplication.java
+++ src/main/java/io/zuolizhu/sportswatchapp/SportswatchappApplication.java
@@ -1,11 +1,7 @@
 package io.zuolizhu.sportswatchapp;
 
-import io.zuolizhu.sportswatchapp.Models.User;
-import io.zuolizhu.sportswatchapp.Repositories.UserRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SportswatchappApplication {
